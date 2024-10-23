47c3b31946beb32c274d87985b944115b63c960e
pratham162u <prathamgohil-inft@atharvacoe.ac.in>
2024-10-23 16:06:25 +0530

    Create Second.py

---
+++ Second.py
@@ -0,0 +1,9 @@
+# This code prints a greeting and performs a simple addition
+print("Hello, World!")
+
+# Simple addition
+a = 5
+b = 3
+result = a + b
+
+print("The sum of", a, "and", b, "is", result)
